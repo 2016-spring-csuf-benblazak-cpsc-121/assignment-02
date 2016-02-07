def14f68f599a1f888eca562fe0ae7f6926040ea
Ben Blazak <benblazak.dev@gmail.com>
2016-02-06 21:18:13 -0800

    working on readme

--- partial-solution/main.cpp
+++ partial-solution/main.cpp
@@ -84,7 +84,6 @@ int main() {
     // animate  neighborhood
     unsigned int size_x = TERM_SIZE_X/Shape::size_x;
     unsigned int size_y = TERM_SIZE_Y/Shape::size_y;
-
     Neighborhood(size_x, size_y).animate(1000);
 
     // ------------------------------------------------------------------------
