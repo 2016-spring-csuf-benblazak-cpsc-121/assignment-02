543468e94a2cbe47eedcd64238f9681302029467
Ben Blazak <benblazak.dev@gmail.com>
2016-02-29 15:11:50 -0800

    finished demo (sans some bug)

--- in-class-example--section-17/test.cpp
+++ in-class-example--section-17/test.cpp
@@ -1,11 +1,23 @@
+#include <chrono>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <random>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 using std::string;
+
+// ----------------------------------------------------------------------------
+
+unsigned int mtrand(unsigned int low, unsigned int high) {
+    static std::random_device rd;
+    static std::mt19937 mt(rd());  // engine (mersenne twister)
+
+    return std::uniform_int_distribution<unsigned int>(low, high)(mt);
+}
 
 // ----------------------------------------------------------------------------
 
@@ -83,6 +95,10 @@ class Shape {
                           ".   .\n"
                           ".....\n";
         }
+
+        bool isHappy() {
+            return false;
+        }
 };
 
 class Neighborhood {
@@ -94,31 +110,73 @@ class Neighborhood {
     public:
         Neighborhood(int sizex, int sizey) : sizex(sizex), sizey(sizey) {
             data = new Shape[sizex * sizey]();
+
+            int counter = 0;
+            double ratiofilled = 0.40;
+            while (counter/double(sizex*sizey) < ratiofilled) {
+                int x = mtrand(0, sizex-1);
+                int y = mtrand(0, sizey-1);
+
+                if (data[ y * sizex + x ].type != "empty")
+                    continue;
+
+                if (mtrand(0,1))
+                    data[ y * sizex + x ] = Shape("triangle");
+                else
+                    data[ y * sizex + x ] = Shape("square");
+
+                counter++;
+            }
         }
 
         ~Neighborhood() {
             delete[] data;
         }
 
+        void move(int x, int y) {
+            for (;;) {
+                int newx = mtrand(0, sizex-1);
+                int newy = mtrand(0, sizey-1);
+
+                if ( data[ newy * sizex + newx ].type != "empty" )
+                    continue;
+
+                data[ newy * sizex + newx ] = data[ y * sizex + x ];
+                data[ y * sizex + x ] = Shape("empty");
+
+                break;
+            }
+        }
+
         void animate(int frames) {
             Buffer b(sizex*Shape::sizex, sizey*Shape::sizey);
 
-            for (int y = 0; y < sizey; y++) {
-                for (int x = 0; x < sizex; x++) {
-                    b.set( x*Shape::sizex,
-                           y*Shape::sizey,
-                           data[ y * sizex + x ].typeart );
+            for (int f = 0; f < frames; f++) {
+                for (int y = 0; y < sizey; y++) {
+                    for (int x = 0; x < sizex; x++) {
+                        b.set( x*Shape::sizex,
+                               y*Shape::sizey,
+                               data[ y * sizex + x ].typeart );
+                    }
                 }
-                cout << endl;
-            }
 
-            b.draw();
+                b.draw();
+
+                for (int y = 0; y < sizey; y++) {
+                    for (int x = 0; x < sizex; x++) {
+                        if ( ! data[ y * sizex + x ].isHappy() )
+                            move(x, y);
+                    }
+                }
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
         }
 };
 
 int main() {
     Neighborhood n(80/Shape::sizex, 25/Shape::sizey);
-    n.animate(1);
+    n.animate(50);
 
     return 0;
 }
