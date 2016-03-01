5b6b2408f23a832a77627ff8d2d6c95c9b5dac11
Ben Blazak <benblazak.dev@gmail.com>
2016-03-01 13:34:20 -0800

    finished in class demo

--- in-class-example--section-01,03/test.cpp
+++ in-class-example--section-01,03/test.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
+#include <random>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 using std::string;
+
+// ----------------------------------------------------------------------------
+
+unsigned int mtrand(unsigned int low, unsigned int high) {
+    static std::random_device rd;
+    static std::mt19937 mt(rd());  // engine (mersenne twister)
+
+    return std::uniform_int_distribution<unsigned int>(low, high)(mt);
+}
 
 // ----------------------------------------------------------------------------
 
@@ -44,23 +56,8 @@ class Buffer {
             data[y * sizex + x] = c;
         }
 
-        char get(int x, int y) {
+        char get(int x, int y) const {
             return data[y * sizex + x];
-        }
-
-        // TODO: move to Shape (because of "incomplete type")
-        void drawShape(int x, int y, const Shape & s) {
-            int originalx = x;
-            int originaly = y;
-            for (int i = 0; i < s.typeart.length(); i++) {
-                if (s.typeart[i] == '\n') {
-                    y++;
-                    x = originalx;
-                } else {
-                    set(x, y, s.typeart[i]);
-                    x++;
-                }
-            }
         }
 };
 
@@ -88,14 +85,100 @@ class Shape {
                           ".....\n";
         }
 
+        void drawToBuffer(Buffer & b, int x, int y) const {
+            int originalx = x;
+            int originaly = y;
+            for (int i = 0; i < typeart.length(); i++) {
+                if (typeart[i] == '\n') {
+                    y++;
+                    x = originalx;
+                } else {
+                    b.set(x, y, typeart[i]);
+                    x++;
+                }
+            }
+        }
 
+        bool isHappy() const {
+            return false;
+        }
+};
+
+class Neighborhood {
+    private:
+        const int sizex;
+        const int sizey;
+        Shape * data;
+
+    public:
+        Neighborhood(int x, int y) : sizex(x), sizey(y) {
+            data = new Shape[ sizex * sizey ]();
+
+            int counter = 0;
+            while (counter/double(sizex*sizey) < 0.40) {
+                int x = mtrand(0, sizex-1);
+                int y = mtrand(0, sizey-1);
+
+                if ( data[ y * sizex + x ].type != "empty" )
+                    continue;
+
+                    if (mtrand(0,1))
+                        data[ y * sizex + x ] = Shape("triangle");
+                    else
+                        data[ y * sizex + x ] = Shape("square");
+
+                    counter++;
+            }
+        }
+
+        ~Neighborhood() {
+            delete[] data;
+        }
+
+        void move(int oldx, int oldy) {
+            for(;;) {
+                int x = mtrand(0, sizex-1);
+                int y = mtrand(0, sizey-1);
+
+                if ( data[ y * sizex + x ].type != "empty" )
+                    continue;
+
+                data[ y * sizex + x ] = data[ oldy * sizex + oldx ];
+                data[ oldy * sizex + oldx ] = Shape("empty");
+
+                break;
+            }
+        }
+
+        void animate(int frames) {
+            Buffer b(sizex * Shape::sizex, sizey * Shape::sizey);
+
+            for (int i = 0; i < frames; i++) {
+                for(int y = 0; y < sizey; y++) {
+                    for(int x = 0; x < sizex; x++) {
+                        data[ y * sizex + x ].drawToBuffer( b,
+                                                            x*Shape::sizex,
+                                                            y*Shape::sizey );
+                    }
+                }
+
+                b.draw();
+
+                for(int y = 0; y < sizey; y++) {
+                    for(int x = 0; x < sizex; x++) {
+                        if (! data[ y * sizex + x ].isHappy())
+                            move(x,y);
+                    }
+                }
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+        }
 };
 
 int main() {
-    Buffer b(80, 25);
-    Shape s("triangle");
-    b.drawShape(5, 10, s);
-    b.draw();
+    Neighborhood n(80/Shape::sizex, 25/Shape::sizey);
+    n.animate(100);
 
     return 0;
 }
